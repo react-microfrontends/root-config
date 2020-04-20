9f7e252acf577e7922cb8bfe9fb4b984bfa598cd
Joel Denning <joeldenning@gmail.com>
2020-04-20 13:08:59 -0600

    Upgrading dependencies, switching to object register

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,46 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = env => ({
-  entry: path.resolve(__dirname, "src/root-config"),
-  output: {
-    filename: "react-mf-root-config.js",
-    libraryTarget: "system",
-    path: path.resolve(__dirname, "dist")
-  },
-  devtool: "sourcemap",
-  module: {
-    rules: [
-      { parser: { system: false } },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [{ loader: "babel-loader" }]
-      }
-    ]
-  },
-  devServer: {
-    historyApiFallback: true,
-    disableHostCheck: true,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    }
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      inject: false,
-      template: "src/index.ejs",
-      templateParameters: {
-        isLocal: env && env.isLocal === "true"
-      }
-    }),
-    new CleanWebpackPlugin()
-  ],
-  externals: ["single-spa", /^@react-mf\/.+$/]
-});
+module.exports = (env) => {
+  const result = {
+    entry: path.resolve(__dirname, "src/root-config"),
+    output: {
+      filename: "react-mf-root-config.js",
+      libraryTarget: "system",
+      path: path.resolve(__dirname, "dist"),
+    },
+    devtool: "sourcemap",
+    module: {
+      rules: [
+        { parser: { system: false } },
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: [{ loader: "babel-loader" }],
+        },
+      ],
+    },
+    devServer: {
+      historyApiFallback: true,
+      disableHostCheck: true,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        inject: false,
+        template: "src/index.ejs",
+        templateParameters: {
+          isLocal: env && env.isLocal === "true",
+        },
+      }),
+      new CleanWebpackPlugin(),
+    ],
+    externals: ["single-spa", /^@react-mf\/.+$/],
+  };
+
+  console.log(result.plugins[0].constructor.name === "HtmlWebpackPlugin");
+
+  return result;
+};
