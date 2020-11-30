5029372b482f81f4ad50804ab051875148c3bcdc
Joel Denning <joeldenning@gmail.com>
2020-11-29 20:29:41 -0700

    Upgrading to webpack 5, switching to pnpm (#21)
    * Upgrading to webpack 5, switching to pnpm
    
    * Fixing build
    
    * Fixing tests
    
    * Self review

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,45 @@
-const path = require("path");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpackMerge = require("webpack-merge");
+const singleSpaDefaults = require("webpack-config-single-spa");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = (env) => {
-  const result = {
-    entry: path.resolve(__dirname, "src/root-config"),
-    output: {
-      filename: "react-mf-root-config.js",
-      libraryTarget: "system",
-      path: path.resolve(__dirname, "dist"),
-    },
-    devtool: "sourcemap",
-    module: {
-      rules: [
-        { parser: { system: false } },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: [{ loader: "babel-loader" }],
-        },
+module.exports = (webpackConfigEnv, argv) => {
+  const orgName = "react-mf";
+  const defaultConfig = singleSpaDefaults({
+    orgName,
+    projectName: "root-config",
+    webpackConfigEnv,
+    argv,
+  });
+
+  const merge = webpackMerge({
+    customizeArray: webpackMerge.unique(
+      "plugins",
+      ["HtmlWebpackPlugin"],
+      (plugin) => plugin.constructor && plugin.constructor.name
+    ),
+  });
+
+  return merge(
+    {
+      plugins: [
+        new HtmlWebpackPlugin({
+          inject: false,
+          template: "src/index.ejs",
+          templateParameters: {
+            isLocal: webpackConfigEnv && webpackConfigEnv.isLocal === "true",
+            orgName,
+          },
+        }),
       ],
     },
-    devServer: {
-      historyApiFallback: true,
-      disableHostCheck: true,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-      https: true,
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        inject: false,
-        template: "src/index.ejs",
-        templateParameters: {
-          isLocal: env && env.isLocal === "true",
+    defaultConfig,
+    {
+      // modify the webpack config however you'd like to by adding to this object
+      devServer: {
+        client: {
+          port: 9000,
         },
-      }),
-      new CleanWebpackPlugin(),
-    ],
-    externals: ["single-spa", /^@react-mf\/.+$/],
-  };
-
-  return result;
+      },
+    }
+  );
 };
