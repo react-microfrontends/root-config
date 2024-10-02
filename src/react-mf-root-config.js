1948aed73ac137fb6e2a90e5358b13acd8fca01f
Joel Denning <joeldenning@gmail.com>
2024-10-02 01:04:28 -0600

    Switch from SystemJS to ESM

--- src/react-mf-root-config.js
+++ src/react-mf-root-config.js
@@ -15,7 +15,7 @@ const routes = constructRoutes(document.querySelector("#single-spa-layout"), {
 });
 const applications = constructApplications({
   routes,
-  loadApp: ({ name }) => System.import(name),
+  loadApp: ({ name }) => import(/* webpackIgnore: true */ name),
 });
 // Delay starting the layout engine until the styleguide CSS is loaded
 const layoutEngine = constructLayoutEngine({
@@ -26,7 +26,7 @@ const layoutEngine = constructLayoutEngine({
 
 applications.forEach(registerApplication);
 
-System.import("@react-mf/styleguide").then(() => {
+import(/* webpackIgnore: true */ "@react-mf/styleguide").then(() => {
   // Activate the layout engine once the styleguide CSS is loaded
   layoutEngine.activate();
   start();
